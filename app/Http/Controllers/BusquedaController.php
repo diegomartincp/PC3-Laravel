ef78dc03bbd60661aecca30bd611ae4ed5210df9
Diego Martín Campos <campos.martin.diego@gmail.com>
2022-05-06 14:18:34 +0200

    Uso de variables de etorno para las rutas

--- app/Http/Controllers/BusquedaController.php
+++ app/Http/Controllers/BusquedaController.php
@@ -1,8 +1,11 @@
 <?php
 
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
+
 
 class BusquedaController extends Controller
 {
@@ -13,12 +16,14 @@ class BusquedaController extends Controller
      */
     public function tweets(Request $request)
     {
+        $RUTA_PYTHON=env('RUTA_PYTHON');
+        $RUTA_CARPETA_LARAVEL=env('RUTA_CARPETA_LARAVEL');
+
         $ciudad = $request->query('ciudad');
         $ciudad_ = str_replace(" ", "+", $ciudad);
 
-        #Precio medio y m2
-        #$result = exec("C:/Users/campo/AppData/Local/Microsoft/WindowsApps/python3.9.exe C:\Users\campo\Documents\GitHub\PC3-Laravel/tweepy_oauthv2_sentiment_analysis_laravel.py " . $ciudad_);
-        $result = exec("C:/Users/Victor/AppData/Local/Microsoft/WindowsApps/python3.9.exe C:\Users\Victor\LARAVEL\PC3-Laravel/tweepy_oauthv2_sentiment_analysis_laravel.py " . $ciudad_);
+        #Llamada python
+        $result = exec($RUTA_PYTHON." ".$RUTA_CARPETA_LARAVEL."/tweepy_oauthv2_sentiment_analysis_laravel.py " . $ciudad_);
         $json = json_decode($result);
 
         return $json;
@@ -32,11 +37,14 @@ class BusquedaController extends Controller
      */
     public function precio(Request $request)
     {
+        $RUTA_PYTHON=env('RUTA_PYTHON');
+        $RUTA_CARPETA_LARAVEL=env('RUTA_CARPETA_LARAVEL');
+
         $ciudad = $request->query('ciudad');
         $ciudad_ = str_replace(" ", "+", $ciudad);
 
-        #Precio medio y m2
-        $result = exec("C:/Users/campo/AppData/Local/Microsoft/WindowsApps/python3.9.exe C:\Users\campo\Documents\GitHub\PC3-Laravel/web_scrapping_precios_laravel.py " . $ciudad_);
+        #Llamada python
+        $result = exec($RUTA_PYTHON." ".$RUTA_CARPETA_LARAVEL."/web_scrapping_precios_laravel.py " . $ciudad_);
         $json = json_decode($result);
 
         return $json;
@@ -50,11 +58,14 @@ class BusquedaController extends Controller
      */
     public function viviendas(Request $request)
     {
+        $RUTA_PYTHON=env('RUTA_PYTHON');
+        $RUTA_CARPETA_LARAVEL=env('RUTA_CARPETA_LARAVEL');
+
         $ciudad = $request->query('ciudad');
         $ciudad_ = str_replace(" ", "+", $ciudad);
 
-        #Precio medio y m2
-        $result = exec("C:/Users/campo/AppData/Local/Microsoft/WindowsApps/python3.9.exe C:\Users\campo\Documents\GitHub\PC3-Laravel/web_scraping_fotocasa_laravel.py " . $ciudad_);
+        #Llamada python
+        $result = exec($RUTA_PYTHON." ".$RUTA_CARPETA_LARAVEL."/web_scraping_fotocasa_laravel.py " . $ciudad_);
         $json = json_decode($result);
 
         return $json;
@@ -67,12 +78,14 @@ class BusquedaController extends Controller
      */
     public function noticias(Request $request)
     {
+        $RUTA_PYTHON=env('RUTA_PYTHON');
+        $RUTA_CARPETA_LARAVEL=env('RUTA_CARPETA_LARAVEL');
+
         $ciudad = $request->query('ciudad');
         $ciudad_ = str_replace(" ", "+", $ciudad);
 
-        #Precio medio y m2
-        #$result = exec("C:/Users/campo/AppData/Local/Microsoft/WindowsApps/python3.9.exe C:\Users\campo\Documents\GitHub\PC3-Laravel/web_scraping_fotocasa_laravel.py " . $ciudad_);
-        $result = exec("C:/Users/Victor/AppData/Local/Microsoft/WindowsApps/python3.9.exe C:\Users\Victor\LARAVEL\PC3-Laravel/prediccion.py " . $ciudad_);
+        #Llamada python
+        $result = exec($RUTA_PYTHON." ".$RUTA_CARPETA_LARAVEL."/prediccion.py " . $ciudad_);
         $json = json_decode($result);
 
         return $json;
